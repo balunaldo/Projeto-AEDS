56500f3c3a68c48b3fb3604c830963d391dc9cb5
Luís Otávio <luistavin22@gmail.com>
2025-11-06 16:02:09 -0300

    adicionando jogo

---
+++ jogoSenha.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <time.h>
+#include "TConj.h"
+
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+    srand(time(NULL));
+
+    TConj senhaGerada;
+    int nvl, maxTentativas, tamanhoSenha;
+
+    printf("Jogo da Senha\n");
+    printf("Níveis: \n");
+    printf("1. Fácil (3 numeros)\n");
+    printf("2. Difícil (4 numeros)\n");
+    printf("3. Nivel de teste (4 numeros, senha visível)\n");
+    
+    printf("Escolha o nível desejado: ");
+    scanf("%d", &nvl);
+    printf("Digite o número máximo de tentativas: ");
+    scanf("%d", &maxTentativas);
+
+    switch(nvl){
+        case 1:
+            tamanhoSenha = 3;
+            senhaGerada = gerar_conjunto(tamanhoSenha);
+            break;
+        case 2:
+            tamanhoSenha = 4;
+            senhaGerada = gerar_conjunto(tamanhoSenha);
+            break;
+        case 3:
+            tamanhoSenha = 4;
+            senhaGerada = gerar_conjunto(tamanhoSenha);
+            printf("Senha gerada: ");
+            imprimir(senhaGerada);
+            break;
+        default:
+            printf("Nível inválido! Encerrando o jogo.\n");
+            return 0;
+    }
+
+    printf("O jogo começou! Tente adivinhar a senha.\n");
+    int tent = 0;
+    int acerto = 0;
+    while(tent < maxTentativas){
+        int numTent;
+        printf("\nTentativa %d -> Digite seu palpite: ", tent + 1);
+        scanf("%d", &numTent);
+
+        TConj palpite = num2conjJogo(numTent, tamanhoSenha);
+
+        int posicaoCorreta = 0;
+        for(int i = 0; i<tamanhoSenha; i++){
+            int digSenha, digPalpite;
+            get_elemento(senhaGerada, i, &digSenha);
+            get_elemento(palpite, i, &digPalpite);
+            if(digSenha == digPalpite){
+                posicaoCorreta++;
+            }            
+        }
+        TConj erros = inter(senhaGerada, palpite);
+        int posicaoErrada = erros.n - posicaoCorreta;
+        liberar_memoria(&erros);
+        if(posicaoCorreta == tamanhoSenha){
+            acerto = 1;
+            liberar_memoria(&palpite);
+            break;
+        }
+        printf("Posição correta(s): %d, Posição errada(s): %d\n", posicaoCorreta, posicaoErrada);
+        liberar_memoria(&palpite);
+        tent++;
+    }
+
+    if(acerto){
+        printf("\nParabéns, você acertou %d tentativas!\n", tent + 1);
+    }else{
+        printf("\nFim de jogo! Você não conseguiu adivinhar a senha.\n");
+        printf("A senha era: ");
+        imprimir(senhaGerada);
+    }
+    liberar_memoria(&senhaGerada);
+    return 0;
+}
